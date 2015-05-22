d710bac854e0d06b7bd42d494b11da0360a4540d
stahl <office@stahlnow.com>
2015-05-22 15:11:30 +0200

    initial commit

---
+++ avr/cores/midicocomake7/main.cpp
@@ -0,0 +1,217 @@
+#include <Arduino.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
+#include "midignusb.h"
+#include "usbdrv.h"
+#include "avr/interrupt.h"
+
+#include "../../../../../libraries/GnusbuinoMIDI/src/GnusbuinoMIDI.h"	// not very clean, I know
+
+// ------------------------------------------------------------------------------
+// - Status Leds
+// ------------------------------------------------------------------------------
+// 							(on means  set to 0 as we sink the LEDs )
+unsigned long blinkstop;
+
+void statusLedOff(StatusLeds led) 		{PORTB &= ~(1 << led); }
+void statusLedOn(StatusLeds led) 		{PORTB |= (1 << led);}
+void statusLedToggle(StatusLeds led)	{PORTB ^= (1 << led);}
+void statusLedBlink(StatusLeds led) {
+    statusLedOn(led);
+    blinkstop = millis()+50;
+}
+
+// ------------------------------------------------------------------------------
+// - Start Bootloader
+// ------------------------------------------------------------------------------
+// dummy function doing the jump to bootloader section (Adress 1C00 on Atmega644)
+
+void startBootloader(void) {
+
+		cli();							// turn off interrupts
+		wdt_disable();					// disable watchdog timer
+		usbDeviceDisconnect(); 			// disconnect gnusb from USB bus
+		_delay_ms(100);
+
+		USB_INTR_ENABLE = 0;
+	    USB_INTR_CFG = 0;       // also reset config bits
+
+		wdt_enable(WDTO_30MS);	// enable watchdog timer
+			while(1) {			// let WDT reset gnusb
+		}
+
+}
+
+
+// ------------------------------------------------------------------------------
+// - usbFunctionDescriptor
+// ------------------------------------------------------------------------------
+
+unsigned char usbFunctionDescriptor(usbRequest_t * rq)
+{
+
+	if (rq->wValue.bytes[1] == USBDESCR_DEVICE) {
+		usbMsgPtr = (unsigned char *) deviceDescrMIDI;
+		return sizeof(deviceDescrMIDI);
+	} else {		/* must be config descriptor */
+		usbMsgPtr = (unsigned char *) configDescrMIDI;
+		return sizeof(configDescrMIDI);
+	}
+}
+
+// ------------------------------------------------------------------------------
+// - usbFunctionSetup
+// ------------------------------------------------------------------------------
+// this function gets called when the usb driver receives a non standard request
+// that is: our own requests defined in ../common/gnusb_cmds.h
+
+unsigned char usbFunctionSetup(unsigned char data[8])
+{
+	void *pVoid = data;
+	usbRequest_t    *rq = static_cast<usbRequest_t*>(pVoid);
+
+	if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {	// class request type
+
+		//  Prepare bulk-in endpoint to respond to early termination
+		if ((rq->bmRequestType & USBRQ_DIR_MASK) ==
+		    USBRQ_DIR_HOST_TO_DEVICE) {}
+	}
+
+	switch (data[1]) {
+        // 								----------------------------   Start Bootloader for reprogramming the gnusb
+		case GNUSBCORE_CMD_START_BOOTLOADER:
+
+			startBootloader();
+			break;
+
+		default:
+			break;
+	}
+
+	return 0xff;
+}
+
+
+// ---------------------------------------------------------------------------
+//  usbFunctionRead
+// ---------------------------------------------------------------------------
+
+unsigned char usbFunctionRead(unsigned char * data, unsigned char len)
+{
+
+//	statusLedToggle(StatusLed_Yellow);
+
+//???? thats from http://cryptomys.de/horo/V-USB-MIDI/index.html
+	data[0] = 0;
+	data[1] = 0;
+	data[2] = 0;
+	data[3] = 0;
+	data[4] = 0;
+	data[5] = 0;
+	data[6] = 0;
+
+	return 7;
+}
+
+
+/*---------------------------------------------------------------------------*/
+/* usbFunctionWrite                                                          */
+/*---------------------------------------------------------------------------*/
+
+unsigned char usbFunctionWrite(unsigned char * data, unsigned char len)
+{
+	return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+/* usbFunctionWriteOut                                                       */
+/*                                                                           */
+/* this Function is called if a MIDI Out message (from PC) arrives.          */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void usbFunctionWriteOut(unsigned char * data, unsigned char len)
+{
+	//statusLedBlink(StatusLed_Yellow);
+
+	while (len >= sizeof(midi_msg)) {
+
+		midi_msg* msg = (midi_msg*)data;
+
+		MIDI.receiveMIDI(msg->byte[0],msg->byte[1],msg->byte[2]);
+
+		data += sizeof(midi_msg);
+		len -= sizeof(midi_msg);
+	}
+}
+
+
+
+// ------------------------------------------------------------------------------
+// - doPeriodical
+// ------------------------------------------------------------------------------
+// stuff that has do be done often in the loop and not forgotten while delaying
+void doPeriodical(void) {
+    /*
+	if (blinkstop) {
+		if (millis() >= blinkstop) {
+			statusLedOff(StatusLed_Yellow);
+			blinkstop = 0;
+		}
+	}
+    */
+
+	usbPoll();
+	MIDI.sendMIDI();
+    wdt_reset();
+}
+
+
+// ------------------------------------------------------------------------------
+// --------------------- Init AD Converter
+// ------------------------------------------------------------------------------
+void adInit(void){
+
+	ADCSRA |= (1 << ADEN);			// enable ADC (turn on ADC power)
+	ADCSRA &= ~(1 << ADATE);		// default to single sample convert mode
+    ADCSRA &= ~(1 << ADIE);         // disable ADC interrupts
+	ADCSRA = ((ADCSRA & ~ADC_PRESCALE_MASK) | ADC_PRESCALE_DIV64);     // Set ADC-Prescaler (-> precision vs. speed)
+
+    ADCSRB &= ~(1 << ADLAR);        // set to right-adjusted result
+
+    // Set ADC Reference Voltage to AVCC
+	ADMUX = 0;
+}
+
+
+// ------------------------------------------------------------------------------
+// - main
+// ------------------------------------------------------------------------------
+int main(void)
+{
+    unsigned char i = 0;
+
+    init();
+    adInit();
+
+    wdt_enable(WDTO_1S);    // enable watchdog timer
+
+    usbInit();
+
+    cli();
+    usbDeviceDisconnect();  // do this while interrupts are disabled
+    while(--i){         // fake USB disconnect for > 250 ms
+        wdt_reset();
+        _delay_ms(1);
+    }
+    usbDeviceConnect();
+
+    sei();
+    setup();
+
+    for (;;) {
+        loop();
+        doPeriodical();
+    }
+
+    return 0;
+}
---
+++ avr/variants/attiny84/pins_arduino.h
@@ -0,0 +1,145 @@
+/*
+  pins_arduino.h - Pin definition functions for ATMEGA644
+  Part of Arduino - http://www.arduino.cc/
+
+  Copyright (c) 2007 David A. Mellis
+  2012 Michael Egger
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+
+  $Id: wiring.h 249 2007-02-03 16:52:51Z mellis $
+*/
+
+
+// ATMEL ATTINY84 / COCOMAKE7
+//
+//                   __  __
+//            VCC  1|° \/  |14  GND
+//   10  XTAL PB0  2|      |13  PA0  A0  0
+//    9  XTAL PB1  3|      |12  PA1  A1  1
+//       RST  PB3  4|      |11  PA2  A2  2
+//   ~8       PB2  5|      |10  PA3  A3  3
+//   ~7  USB- PA7  6|      |9   PA4  A4  4
+//   ~6  USB+ PA6  7|      |8   PA5  A5  5~
+//                   ------
+
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+
+#include <avr/pgmspace.h>
+
+#define NUM_DIGITAL_PINS            11
+#define NUM_ANALOG_INPUTS           8
+#define analogInputToDigitalPin(p)  (p)
+#define digitalPinHasPWM(p)         ((p==5) || (p==8))  // (p==6)) || (p==7)
+
+static const uint8_t SS   = 7; // could also be 2
+static const uint8_t MOSI = 6;
+static const uint8_t MISO = 5;
+static const uint8_t SCK  = 4;
+
+static const uint8_t SDA = 6;
+static const uint8_t SCL = 4;
+static const uint8_t LED_BUILTIN = 8;
+
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
+static const uint8_t A6 = 6;
+static const uint8_t A7 = 7;
+
+
+#ifdef ARDUINO_MAIN
+
+
+#define PB 1
+
+
+const uint16_t PROGMEM port_to_mode_PGM[] = {
+	NOT_A_PORT,
+    (uint8_t) &DDRA,
+	(uint8_t) &DDRB,
+};
+
+const uint16_t PROGMEM port_to_output_PGM[] = {
+	NOT_A_PORT,
+    (uint8_t) &PORTA,
+	(uint8_t) &PORTB,
+};
+
+const uint16_t PROGMEM port_to_input_PGM[] = {
+	NOT_A_PORT,
+    (uint8_t) &PINA,
+	(uint8_t) &PINB,
+};
+
+const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
+	// PORTLIST
+	// -------------------------------------------
+	PA, // 0
+    PA,
+    PA,
+    PA,
+    PA,
+    PA,
+    PA,
+    PA,
+    PB, // 8
+    PB,
+    PB,
+};
+
+//ok
+const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
+	// PIN IN PORT
+	// -------------------------------------------
+	_BV(0), // port A
+    _BV(1),
+    _BV(2),
+    _BV(3),
+    _BV(4),
+    _BV(5),
+    _BV(6),
+    _BV(7),
+    _BV(2), // port B
+    _BV(1),
+    _BV(0),
+};
+
+//ok
+const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
+	// TIMERS
+	// -------------------------------------------
+	NOT_ON_TIMER,
+    NOT_ON_TIMER,
+    NOT_ON_TIMER,
+    NOT_ON_TIMER,
+    NOT_ON_TIMER,
+    TIMER1B, // OC1B
+    TIMER1A, // OC1A
+    TIMER0B, // OC0B
+    TIMER0A, // OC0A
+    NOT_ON_TIMER,
+    NOT_ON_TIMER,
+};
+
+#endif
+
+#endif
